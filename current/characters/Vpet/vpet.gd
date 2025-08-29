83c7bcb2ac2acb8af778abdbcf5e95eca16f7077
Brian Huynh <brian123huy@gmail.com>
2025-08-28 22:16:56 -0400

    Walking / Jump Emotes
    Added walking and jumping :D

--- current/characters/Vpet/vpet.gd
+++ current/characters/Vpet/vpet.gd
@@ -2,15 +2,26 @@ extends Node2D
 
 
 var moving: bool = false
-var current_animation: String = ""
+var current_animation: String = "idle"
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _physics_process(_delta: float) -> void:
-	if not moving and current_animation == "":
+	if current_animation == "idle":
 		if rng.randi_range(0, 299) == 0:
 			moving = true
+			if rng.randi_range(0, 1) == 0:
+				current_animation = "jump"
+			else:
+				current_animation = "walk"
+			$AnimatedSprite2D.play(current_animation)
+			var next_x_pos: int = rng.randi_range(0, 1080)
+			if next_x_pos > global_position.x:
+				scale.x = 1.0
+			else:
+				scale.x = -1.0
 			await create_tween().tween_property(self, "global_position:x", rng.randi_range(0, 1080), 5.0).finished
-			moving = false
+			current_animation = "idle"
+			$AnimatedSprite2D.play(current_animation)
 		elif rng.randi_range(0, 299) == 0:
 			match randi_range(0, 6):
 				0:
@@ -27,11 +38,11 @@ func _physics_process(_delta: float) -> void:
 					current_animation = "love"
 				6:
 					current_animation = "sick"
-			$AnimatedSprite2D.play(current_animation)
+		$AnimatedSprite2D.play(current_animation)
 	
 	global_position= global_position.clamp(Vector2(0, 0), Vector2(1080, 1600))
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	current_animation = ""
-	$AnimatedSprite2D.play("idle")
+	current_animation = "idle"
+	$AnimatedSprite2D.play(current_animation)
